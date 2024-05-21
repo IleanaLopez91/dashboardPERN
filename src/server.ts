0dea759b67715bb0c148b5a2469f1b5c607e4548
Ileana <ferchu91garcia@gmail.com>
2024-05-20 21:49:19 -0300

    implementando cors y morgan

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./router";
 import db from "./config/db";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
 
@@ -21,7 +23,21 @@ export async function connectDB() {
 connectDB();
 
 const server = express();
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
+
 server.use(express.json());
+
+server.use(morgan("dev"));
 
 server.use("/api/products", router);
 
